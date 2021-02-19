0d5ae32284d6ea1d1d5451e1a6d6aea101c9e2cc
Manolis <gkouem313@gmail.com>
2021-02-19 20:24:16 +0200

    implement routing

--- core/Router.php
+++ core/Router.php
@@ -4,8 +4,35 @@ namespace app\core;
 
 class Router
 {
-    public function __construct()
+    public Request $request;
+
+    public function __construct(Request $request)
     {
-        
+        $this->request = $request;
+    }
+
+    protected array $routes = [];
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+
+        if ($callback === false) {
+            echo 'Not found';
+            exit;
+        }
+
+        echo call_user_func($callback);
+
+        // echo '<pre>';
+        // var_dump($this->routes);
+        // echo '<pre />';
     }
 }
